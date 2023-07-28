91e23fb8d3f67eac76b8f8ac8246c3646e32628c
Theophilus E.K Adukpo <theoadu@gmail.com>
2023-07-28 21:06:41 +0000

    add openaicontroller

--- config/openaiConfig.js
+++ config/openaiConfig.js
@@ -1,8 +1,10 @@
 const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config;
+require('dotenv').config();
+
+console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);

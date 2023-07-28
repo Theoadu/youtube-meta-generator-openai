7db460e1dfd107a7752ed3841fdc370f391ec083
Theophilus E.K Adukpo <theoadu@gmail.com>
2023-07-28 23:37:36 +0000

    add a simple frontend

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ const app = express();
 // middleware
 
 app.use(express.json());
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 
 // routes
-app.post('/openai/meta', generateMeta)
-app.post('/openai/image', generateImage)
+app.post('/openai/meta', generateMeta);
+app.post('/openai/image', generateImage);
 
 app.listen(4000, () => console.log('listening for request on port 4000'));
---
+++ public/index.js
@@ -0,0 +1,36 @@
+// forms
+const metaForm = document.querySelector('.meta-form');
+const imageForm = document.querySelector('.image-form');
+
+// output elements
+const description = document.querySelector('.description p');
+const tags = document.querySelector('.tags p');
+const thumbnail = document.querySelector('.thumbnail img');
+
+// description and tags
+metaForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const res = await fetch('/openai/meta', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: metaForm.title.value }),
+  });
+  const data = await res.json();
+
+  description.textContent = data.description.content;
+  tags.textContent = data.tags.content;
+});
+
+imageForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const res = await fetch('/openai/image', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      imageDescription: imageForm.imageDescription.value,
+    }),
+  });
+  const data = await res.json();
+
+  thumbnail.setAttribute('src', data.url);
+});
